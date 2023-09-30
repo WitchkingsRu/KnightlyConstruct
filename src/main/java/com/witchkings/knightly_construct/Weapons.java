5d516e759ce806cf7ba087018489a6cc94ca2f9f
Witchkings <vovakla647@gmail.com>
2023-09-30 14:20:24 +0300

    added some classes

--- src/main/java/com/witchkings/knightly_construct/Weapons.java
+++ src/main/java/com/witchkings/knightly_construct/Weapons.java
@@ -1,6 +1,110 @@
 package com.witchkings.knightly_construct;
 
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ArrowItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.registries.RegistryObject;
+import slimeknights.mantle.registration.object.EnumObject;
+import slimeknights.mantle.registration.object.ItemObject;
+import slimeknights.mantle.util.SupplierCreativeTab;
+import slimeknights.tconstruct.TConstruct;
+import slimeknights.tconstruct.common.TinkerModule;
+import slimeknights.tconstruct.common.config.Config;
+import slimeknights.tconstruct.common.config.ConfigurableAction;
+import slimeknights.tconstruct.common.data.tags.MaterialTagProvider;
+import slimeknights.tconstruct.library.client.data.material.GeneratorPartTextureJsonGenerator;
+import slimeknights.tconstruct.library.client.data.material.MaterialPartTextureGenerator;
+import slimeknights.tconstruct.library.json.AddToolDataFunction;
+import slimeknights.tconstruct.library.json.RandomMaterial;
+import slimeknights.tconstruct.library.modifiers.TinkerHooks;
+import slimeknights.tconstruct.library.tools.IndestructibleItemEntity;
+import slimeknights.tconstruct.library.tools.SlotType;
+import slimeknights.tconstruct.library.tools.ToolPredicate;
+import slimeknights.tconstruct.library.tools.capability.ToolCapabilityProvider;
+import slimeknights.tconstruct.library.tools.capability.ToolFluidCapability;
+import slimeknights.tconstruct.library.tools.capability.ToolInventoryCapability;
+import slimeknights.tconstruct.library.tools.definition.aoe.BoxAOEIterator;
+import slimeknights.tconstruct.library.tools.definition.aoe.CircleAOEIterator;
+import slimeknights.tconstruct.library.tools.definition.aoe.FallbackAOEIterator;
+import slimeknights.tconstruct.library.tools.definition.aoe.IAreaOfEffectIterator;
+import slimeknights.tconstruct.library.tools.definition.aoe.TreeAOEIterator;
+import slimeknights.tconstruct.library.tools.definition.aoe.VeiningAOEIterator;
+import slimeknights.tconstruct.library.tools.definition.harvest.FixedTierHarvestLogic;
+import slimeknights.tconstruct.library.tools.definition.harvest.IHarvestLogic;
+import slimeknights.tconstruct.library.tools.definition.harvest.ModifiedHarvestLogic;
+import slimeknights.tconstruct.library.tools.definition.harvest.TagHarvestLogic;
+import slimeknights.tconstruct.library.tools.definition.module.IToolModule;
+import slimeknights.tconstruct.library.tools.definition.module.ToolModuleHooks;
+import slimeknights.tconstruct.library.tools.definition.module.interaction.DualOptionInteraction;
+import slimeknights.tconstruct.library.tools.definition.module.interaction.PreferenceSetInteraction;
+import slimeknights.tconstruct.library.tools.definition.weapon.CircleWeaponAttack;
+import slimeknights.tconstruct.library.tools.definition.weapon.IWeaponAttack;
+import slimeknights.tconstruct.library.tools.definition.weapon.ParticleWeaponAttack;
+import slimeknights.tconstruct.library.tools.definition.weapon.SweepWeaponAttack;
+import slimeknights.tconstruct.library.tools.helper.ModifierLootingHandler;
+import slimeknights.tconstruct.library.tools.item.ModifiableArmorItem;
+import slimeknights.tconstruct.library.tools.item.ModifiableItem;
+import slimeknights.tconstruct.library.tools.item.ModifiableLauncherItem;
+import slimeknights.tconstruct.library.tools.item.ModifiableStaffItem;
+import slimeknights.tconstruct.library.utils.BlockSideHitListener;
+import slimeknights.tconstruct.tools.TinkerTools;
+import slimeknights.tconstruct.tools.ToolDefinitions;
+import slimeknights.tconstruct.tools.data.StationSlotLayoutProvider;
+import slimeknights.tconstruct.tools.data.ToolDefinitionDataProvider;
+import slimeknights.tconstruct.tools.data.ToolsRecipeProvider;
+import slimeknights.tconstruct.tools.data.material.MaterialDataProvider;
+import slimeknights.tconstruct.tools.data.material.MaterialRecipeProvider;
+import slimeknights.tconstruct.tools.data.material.MaterialRenderInfoProvider;
+import slimeknights.tconstruct.tools.data.material.MaterialStatsDataProvider;
+import slimeknights.tconstruct.tools.data.material.MaterialTraitsDataProvider;
+import slimeknights.tconstruct.tools.data.sprite.TinkerMaterialSpriteProvider;
+import slimeknights.tconstruct.tools.data.sprite.TinkerPartSpriteProvider;
+import slimeknights.tconstruct.tools.item.ArmorSlotType;
+import slimeknights.tconstruct.tools.item.CrystalshotItem;
+import slimeknights.tconstruct.tools.item.CrystalshotItem.CrystalshotEntity;
+import slimeknights.tconstruct.tools.item.ModifiableBowItem;
+import slimeknights.tconstruct.tools.item.ModifiableCrossbowItem;
+import slimeknights.tconstruct.tools.item.ModifiableDaggerItem;
+import slimeknights.tconstruct.tools.item.ModifiableSwordItem;
+import slimeknights.tconstruct.tools.item.PlateArmorItem;
+import slimeknights.tconstruct.tools.item.SlimelytraItem;
+import slimeknights.tconstruct.tools.item.SlimeskullItem;
+import slimeknights.tconstruct.tools.item.SlimesuitItem;
+import slimeknights.tconstruct.tools.item.TravelersGearItem;
+import slimeknights.tconstruct.tools.logic.EquipmentChangeWatcher;
+import slimeknights.tconstruct.tools.menu.ToolContainerMenu;
 
-public class Weapons {
+public final class Weapons extends TinkerModule {
+    public TinkerTools() {
+        SlotType.init();
+        BlockSideHitListener.init();
+        ModifierLootingHandler.init();
+        RandomMaterial.init();
+    }
 
+    /** Creative tab for all tool items */
+    public static final CreativeModeTab TAB_TOOLS = new SupplierCreativeTab(TConstruct.MOD_ID, "tools", () -> TinkerTools.pickaxe.get().getRenderTool());
+
+    /** Loot function type for tool add data */
+    public static final RegistryObject<LootItemFunctionType> lootAddToolData = LOOT_FUNCTIONS.register("add_tool_data", () -> new LootItemFunctionType(AddToolDataFunction.SERIALIZER));
+
+    /*
+     * Items
+     */
+    private static final Item.Properties TOOL = new Item.Properties().stacksTo(1).tab(TAB_TOOLS);
+
+    public static final ItemObject<ModifiableItem> longsword = ITEMS.register("longsword", () -> new ModifiableItem(TOOL, ToolDefinitions.PICKAXE));
 }
